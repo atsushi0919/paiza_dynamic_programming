6b0d70751b3304c536fd0b28bba9bac7cedb6156
atsushi0919 <hatake0919@yahoo.co.jp>
2021-04-05 12:11:01 +0900

    add/dp5-01

---
+++ dp5-01.rb
@@ -0,0 +1,30 @@
+# 最長部分増加列 (paizaランク B 相当)
+# https://paiza.jp/works/mondai/dp_primer/dp_primer_lis_step0
+
+def solve(input_data)
+  n, *a = input_data.split("\n").map(&:to_i)
+
+  dp = [1]
+  (1...n).each do |i|
+    dp[i] = 1  # 木 i のみからなる部分列の長さ
+    (0...i).each do |j|
+      if a[j] < a[i]
+        dp[i] = [dp[i], dp[j] + 1].max  # 最後が木 j であるような増加部分列の末尾に木 i をくっつける
+      end
+    end
+  end
+  dp
+end
+
+#puts solve(STDIN.read)
+
+in1 = <<~"EOS"
+  5
+  100
+  102
+  101
+  91
+  1999
+EOS
+ans1 = 3
+p solve(in1)

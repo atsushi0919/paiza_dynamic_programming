9d73455c98fb6174769d163a374e2634fec8237f
atsushi0919 <hatake0919@yahoo.co.jp>
2021-10-04 13:10:49 +0900

    dp1 投稿用の確認・編集

--- dp1-03.rb
+++ dp1-03.rb
@@ -1,25 +1,98 @@
 # 特殊な2項間漸化式 1 (paizaランク B 相当)
 # https://paiza.jp/works/mondai/dp_primer/dp_primer_recursive_formula_step2
 
+INPUT1 = <<~"EOS"
+  5 -7 10 5
+EOS
+OUTPUT1 = <<~"EOS"
+  11
+EOS
+
 def solve(input_data)
   x, d1, d2, k = input_data.split.map(&:to_i)
 
-  a = [x]
-  while a.size < k
-    if a.size.even?
-      a << a[-1] + d1
+  # n = 1 : x
+  dp = [x]
+
+  # n >= 2
+  (k - 1).times do
+    dp << if dp.size.even?
+      # 項が偶数のとき
+      dp[-1] + d1
     else
-      a << a[-1] + d2
+      # 項が奇数のとき
+      dp[-1] + d2
     end
   end
-  a[-1]
+
+  # k 項の値
+  dp[-1]
 end
 
-#puts solve(STDIN.read)
+puts solve(STDIN.read)
 
-in1 = "5 -7 10 5"
-res1 = 11
+# 確認用コード
+# p solve(INPUT1)
+# > 11
 
-in2 = "-675 464 153 146"
-res2 = 43902
-puts solve(in1)
+INPUT2 = <<~"EOS"
+  -675 464 153 146
+EOS
+OUTPUT2 = <<~"EOS"
+  43902
+EOS
+
+=begin
+特殊な2項間漸化式 1 (paizaランク B 相当)
+問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
+
+シェア用URL:
+https://paiza.jp/works/mondai/dp_primer/dp_primer_recursive_formula_step2
+問題文のURLをコピーする
+ チャレンジする言語
+
+得意な言語を選択してください
+コードを書いて解いてみる
+ チケット使用済
+問題
+ 下記の問題をプログラミングしてみよう！
+整数 x, d_1, d_2, k が与えられます。
+次のように定められた数列の k 項目の値を出力してください。
+
+・ a_1 = x 
+・ a_n = a_{n-1} + d_1 (n が奇数のとき、n ≧ 3) 
+・ a_n = a_{n-1} + d_2 (n が偶数のとき)
+(ヒント)
+添字の偶奇によって漸化式の形が変わっていますが、やることはこれまでと同じです。
+a_1 ~ a_{k-1} が求まっているとして、a_k をどのように計算すればよいかを考えてみましょう。
+計算するときに、添字の偶奇による場合分けを行えばよいです。
+
+入力される値
+x d_1 d_2 k
+
+入力値最終行の末尾に改行が１つ入ります。
+文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
+期待する出力
+数列の k 項目の値を出力してください。
+
+また、末尾に改行を入れ、余計な文字、空行を含んではいけません。
+
+
+a_k
+条件
+すべてのテストケースにおいて、以下の条件をみたします。
+
+・ -1,000 ≦ x ≦ 1,000
+
+・ -1,000 ≦ d_1 ≦ 1,000
+
+・ -1,000 ≦ d_2 ≦ 1,000
+
+・ 1 ≦ k ≦ 1,000
+
+入力例1
+5 -7 10 5
+
+出力例1
+11
+=end
--- fibo1.rb
+++ fibo1.rb
@@ -1,26 +1,24 @@
 def fib1(n)
   # n = 0 -> return 0
   # n = 1 -> return 1
-  if n < 2
-    puts "return #{n}"
-    return n
-  end
+  return n if n < 2
 
   # n が 0 か 1 になるまで再起呼び出しを行う
   a = fib1(n - 1)
   b = fib1(n - 2)
-  c = a + b
-  puts "return #{a} + #{b} = #{c}"
-  return c
+
+  # 返り値を足して返す
+  a + b
 end
 
-# p fib1(4)
+p fib1(4)
 # > 3
 
-# start = Time.now
-# p fib1(40)
+# 第40項を計算した時に掛かる時間
+start = Time.now
+p fib1(40)
 # > 102334155
-# puts Time.now - start
+puts Time.now - start
 # > 14.8706037
 
 def fib2(n)
@@ -39,13 +37,15 @@ def fib2(n)
   $memo[n] = c
 end
 
-#$memo = [0, 1]
-#p fib2(4)
+# グローバル変数に答えを記録
+$memo = [0, 1]
+p fib2(4)
+# > 3
 
-#start = Time.now
-#p fib2(40)
+start = Time.now
+p fib2(40)
 # > 102334155
-#puts Time.now - start
+puts Time.now - start
 # > 8.61e-05
 
 def fib3(n)
@@ -54,20 +54,23 @@ def fib3(n)
 
   ary = fib3(n - 1)
   ary << ary[-1] + ary[-2]
+  # p ary
+
+  ary
 end
 
-#p fib3(4)[-1]
+p fib3(4)[-1]
 # > 3
 
-#start = Time.now
-#p fib3(40)[-1]
+start = Time.now
+p fib3(40)[-1]
 # > 102334155
-#puts Time.now - start
+puts Time.now - start
 # > 8.15e-05
 
 def fib4(n)
   dp = [0, 1]
-  dp[n] if n < 2
+  return dp[..n] if n < 2
 
   (n - 1).times do
     dp << dp[-1] + dp[-2]
@@ -75,7 +78,7 @@ def fib4(n)
   dp
 end
 
-# p fib4(4)[-1]
+p fib4(4)[-1]
 # > 3
 
 start = Time.now
@@ -87,18 +90,18 @@ puts Time.now - start
 def fib5(n)
   return n if n < 2
   a, b = 0, 1
+
   n.times do
     a, b = b, a + b
-    # puts "a: #{a}, b: #{b}"
   end
   a
 end
 
-#p fib4(4)
+p fib4(4)
 # > 3
 
-#start = Time.now
-#p fib5(40)
+start = Time.now
+p fib5(40)
 # > 102334155
-#puts Time.now - start
+puts Time.now - start
 # > 8.85e-05
